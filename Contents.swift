80734f3368174e0530a6d6994fa1023de68c004e
asalas1093 <AlejandroSalas@Alejandros-MacBook-Pro.local>
2015-09-21 19:19:48 -0500

    AlejandroS_CriticalThinking

---
+++ Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var numA :Int = 4
+var numB :Int = 6
+var numC :Int = 10
+let valueA = "Value A ="
+let maxValue = numC * numC
+
+
+if numA < numB {
+    ++numA
+}
+
+else {
+    while numA < maxValue{
+        ++numA
+    }
+
+}
+
+
+println("Your Values are listed below: ")
+println("Value A = \(numA)")
+println("Value B = \(numB)")
+println("Value C = \(numC)")
